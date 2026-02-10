edadf889425dd7b091faa6b7ff3ab94a8c532018
Miguel Rodriguez Perez <miguelrp@ext.inditex.com>
2026-02-10 23:19:26 +0100

    [main] adds seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bentoed.vercel.app",
-  integrations: [tailwind()],
   output: "static",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+  integrations: [tailwind(), sitemap()],
 });
